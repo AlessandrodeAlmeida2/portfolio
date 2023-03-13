6c2f575fc6f245ee3e39605f2fdfd107fa79a945
Alessandro de Almeida <124206367+AlessandrodeAlmeida2@users.noreply.github.com>
2023-03-13 15:30:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ var typed = new Typed(".multiple-text",{
     backDelay: 1000,
     loop: true
 })
+
+// Adicione um evento de clique ao botão de menu hambúrguer para alternar a visibilidade do menu
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+});
