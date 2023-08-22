1e6b8b8c9529a819b13685260493acca024cd086
Alessandro de Almeida <124206367+AlessandrodeAlmeida2@users.noreply.github.com>
2023-08-22 10:04:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text",{
-    strings: ["Frontend Developer", "Data Scientist"],
+    strings: ["Full-Stack Developer", "Data Scientist"],
     typeSpeed: 100,
     backSpeed:100,
     backDelay: 1000,
